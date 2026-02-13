5e734b7453c953c37a40436182b6bc2d9a68760f
Bhargavi49 <bhargavi.desh2411@gmail.com>
2026-02-14 00:16:11 +0530

    Day 1: Clean project structure at root

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "../include/utils.h"
+
+int main(){
+
+    printWelcome();
+    return 0;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/utils.h"
+using namespace std;
+void printWelcome(){
+   cout<<"_________________\n";
+   cout<<"Welcome to MINI-REDIS\n";
+   cout<<"__________________\n";
+
+}
